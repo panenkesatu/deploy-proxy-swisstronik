51fad128712e3d7c1f5c3dfe4a22ac2086284d44
jasakocok <followerske1@gmail.com>
2024-07-30 17:36:21 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xF191f97b7131dDe4B06c9D7868F0173A0236f5c4'
+export const ProxyAdmin = '0x174B8C479B69b0E050A825cF5ec1f935CDaBC4ea'
+export const SWTRImplementation = '0x8E428BDc5F5DD03FA04C743aaA3e74aF713b05Fe'
     
